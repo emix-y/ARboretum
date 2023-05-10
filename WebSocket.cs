140bdfef2bbf69de2d4b56270079c86d61efcde8
emily clark <93390989+emix-y@users.noreply.github.com>
2023-05-10 01:04:03 +0100

    Add files via upload
    websocket unity

---
+++ WebSocket.cs
@@ -0,0 +1,35 @@
+using UnityEditor.ShaderKeywordFilter;
+using UnityEngine;
+using WebSocketSharp;
+public class WsClient : MonoBehaviour
+{
+    WebSocket ws;
+
+    public float humidity = 1.0f;
+    public float temp = 1.0f;
+
+    public string url = "192.168.8.101:80";
+    private void Start()
+    {
+        ws = new WebSocket("ws://"+url);
+        ws.Connect();
+        ws.OnMessage += (sender, e) =>
+        {
+            string[] StringValues = e.Data.Split(new string[] { "," }, System.StringSplitOptions.RemoveEmptyEntries);
+            humidity = float.Parse(StringValues[0]);
+            temp = float.Parse(StringValues[1]);
+        };
+    }
+    private void Update()
+    {
+        if (ws == null)
+        {
+            return;
+        }
+        if (Time.frameCount % 2500 ==0)
+        {
+    
+            ws.Send("Hello");
+        }
+    }
+}
